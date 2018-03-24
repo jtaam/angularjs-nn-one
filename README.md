4d5503367945a093f6d068e069e8c155661506b0
dev-tam <tamjid.mdh@gmail.com>
2018-03-25 05:16:41 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# angularjs-nn-one
---
+++ app/app.js
@@ -0,0 +1,73 @@
+var myNinjaApp = angular.module('fkWhites',['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/home',{
+      templateUrl:'views/home.html'
+    })
+    .when('/directory', {
+      templateUrl:'views/directory.html',
+      controller:'NinjaController'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+}]);
+
+myNinjaApp.controller('NinjaController',['$scope', function($scope){
+  $scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1);
+  };
+
+  $scope.addNinja = function(){
+      $scope.ninjas.push({
+        name: $scope.newNinja.name,
+        belt:$scope.newNinja.belt,
+        rate:parseInt($scope.newNinja.rate),
+        available:true
+      });
+      $scope.newNinja.name = "";
+      $scope.newNinja.belt = "";
+      $scope.newNinja.rate = "";
+  };
+
+  $scope.ninjas = [
+    {
+      name:'Yoshi',
+      belt:'green',
+      rate:50,
+      available:true,
+      thumb:"content/img/yoshi.png"
+    },
+    {
+      name:'Crystal',
+      belt:'yellow',
+      rate:30,
+      available:true,
+      thumb:"content/img/crystal.png"
+    },
+    {
+      name:'Ryu',
+      belt:'orange',
+      rate:10,
+      available:false,
+      thumb:"content/img/ryu.png"
+    },
+    {
+      name:'Shaun',
+      belt:'black',
+      rate:1000,
+      available:true,
+      thumb:"content/img/shaun.png"
+    }
+  ];
+  // $scope.message = "hey lorem";
+  // $scope.ninjas = ['alpha','beta','charli','delta'];
+}]);
+// myNinjaApp.config(function({
+//
+// }));
+// myNinjaApp.run(function(){
+//
+// });

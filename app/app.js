2014e514f5c5603cca34a0b44af3143c63b3288d
dev-tam <tamjid.mdh@gmail.com>
2018-03-25 06:05:49 +0600

    custom directive

--- app/app.js
+++ app/app.js
@@ -2,66 +2,56 @@ var myNinjaApp = angular.module('fkWhites',['ngRoute']);
 
 myNinjaApp.config(['$routeProvider', function($routeProvider){
   $routeProvider
-    .when('/home',{
-      templateUrl:'views/home.html'
-    })
-    .when('/directory', {
-      templateUrl:'views/directory.html',
-      controller:'NinjaController'
-    })
-    .otherwise({
-      redirectTo: '/home'
-    });
+  .when('/home',{
+    templateUrl:'views/home.html',
+    controller:'NinjaController'
+  })
+  .when('/directory', {
+    templateUrl:'views/directory.html',
+    controller:'NinjaController'
+  })
+  .otherwise({
+    redirectTo: '/home'
+  });
 }]);
-
-myNinjaApp.controller('NinjaController',['$scope', function($scope){
+// Directive
+myNinjaApp.directive('randomNinja',[function(){
+  return {
+    restrict:'E',
+    scope:{
+      ninjas:'=', // binding
+      title:'='
+    },
+    templateUrl: 'views/random.html',
+    controller:function($scope){
+      $scope.random = Math.floor(Math.random()*4);
+    }
+  };
+}]);
+// Controller
+myNinjaApp.controller('NinjaController',['$scope', '$http',function($scope, $http){
   $scope.removeNinja = function(ninja){
     var removedNinja = $scope.ninjas.indexOf(ninja);
     $scope.ninjas.splice(removedNinja, 1);
   };
 
   $scope.addNinja = function(){
-      $scope.ninjas.push({
-        name: $scope.newNinja.name,
-        belt:$scope.newNinja.belt,
-        rate:parseInt($scope.newNinja.rate),
-        available:true
-      });
-      $scope.newNinja.name = "";
-      $scope.newNinja.belt = "";
-      $scope.newNinja.rate = "";
+    $scope.ninjas.push({
+      name: $scope.newNinja.name,
+      belt:$scope.newNinja.belt,
+      rate:parseInt($scope.newNinja.rate),
+      available:true
+    });
+    $scope.newNinja.name = "";
+    $scope.newNinja.belt = "";
+    $scope.newNinja.rate = "";
   };
 
-  $scope.ninjas = [
-    {
-      name:'Yoshi',
-      belt:'green',
-      rate:50,
-      available:true,
-      thumb:"content/img/yoshi.png"
-    },
-    {
-      name:'Crystal',
-      belt:'yellow',
-      rate:30,
-      available:true,
-      thumb:"content/img/crystal.png"
-    },
-    {
-      name:'Ryu',
-      belt:'orange',
-      rate:10,
-      available:false,
-      thumb:"content/img/ryu.png"
-    },
-    {
-      name:'Shaun',
-      belt:'black',
-      rate:1000,
-      available:true,
-      thumb:"content/img/shaun.png"
-    }
-  ];
+  // Get Data from JSON
+  $http.get('data/ninjas.json').success(function(data){
+    $scope.ninjas = data;
+  });
+  // console.log(angular.toJson($scope.ninjas));
   // $scope.message = "hey lorem";
   // $scope.ninjas = ['alpha','beta','charli','delta'];
 }]);
